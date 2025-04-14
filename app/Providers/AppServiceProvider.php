0288e169db31e188ba59347f6090df69183ea224
Rahmat <rahmat.ullah@deltadots.com>
2025-04-14 12:08:11 +0500

    db destructive commands restricted on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Console\Events\CommandStarting;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (App::isLocal()) {
+            Schema::defaultStringLength(191);
+        }
+
+        if (App::isProduction()) {
+            // Listen for Artisan commands
+            Event::listen(CommandStarting::class, function (CommandStarting $event) {
+                $destructiveCommands = [
+                    'migrate:fresh',
+                    'migrate:reset',
+                    'migrate:refresh',
+                    'db:wipe',
+                ];
+    
+                if (in_array($event->command, $destructiveCommands)) {
+                    throw new \RuntimeException("The '{$event->command}' command is not allowed in production.");
+                }
+            });
+        }
     }
 }
