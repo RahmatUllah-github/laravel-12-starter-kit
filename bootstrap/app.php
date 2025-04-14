8d34c1b95540c5fd60a01565dfe706c9a767e69d
Rahmat <rahmat.ullah@deltadots.com>
2025-04-14 15:18:23 +0500

    exceptions handled

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,48 @@
 <?php
 
+use App\Http\Middleware\ForceJsonRequestHeader;
+use App\Services\JsonResponseService;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api(prepend: ForceJsonRequestHeader::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return JsonResponseService::notFoundErrorResponse('Please login first.');
+            }
+        });
+
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return JsonResponseService::notFoundErrorResponse('Route not found.');
+            }
+        });
+
+        $exceptions->render(function (ModelNotFoundException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return JsonResponseService::notFoundErrorResponse(class_basename($e->getModel()) . ' not found.');
+            }
+        });
+
+        $exceptions->render(function (AuthorizationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return JsonResponseService::forbiddenErrorResponse('You do not have permission to perform this action.');
+            }
+        });
     })->create();
